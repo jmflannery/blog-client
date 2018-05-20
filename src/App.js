044bb8bc06215311608005acc44c88399bee6538
Jack Flannery <jmflannery81@gmail.com>
2018-05-20 00:12:15 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Home from './Home';
+import Header from './Header';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Header />
+        <Router>
+          <Route exact path="/" component={Home} />
+        </Router>
+      </div>
+    );
+  }
+}
+
+export default App;
