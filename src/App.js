0c679bfb34ca78658cdee2ff1171c36256567f11
Jack Flannery <jmflannery81@gmail.com>
2018-05-20 20:42:25 -0400

    Add redux

--- src/App.js
+++ src/App.js
@@ -1,21 +1,27 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import { Provider } from 'react-redux'
+import configureStore from './configure-store';
 import Home from './Home';
 import Header from './Header';
 // import logo from './logo.svg';
 import './App.css';
 
+const store = configureStore();
+
 class App extends Component {
   render() {
     return (
-      <div>
-        <Header />
-        <Router>
-          <Route exact path="/" component={Home} />
-        </Router>
-      </div>
+      <Provider store={store}>
+        <div>
+          <Header />
+          <Router>
+            <Route exact path="/" component={Home} />
+          </Router>
+        </div>
+      </Provider>
     );
   }
-}
+};
 
 export default App;
