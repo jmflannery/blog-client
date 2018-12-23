7b69575c259278c43b7b41a2ae4260909696acdc
Jack Flannery <jmflannery81@gmail.com>
2018-12-23 01:28:12 -0500

    Update bottom nav style and layout

--- src/components/posts/bottom-nav/bottom-nav-component.js
+++ src/components/posts/bottom-nav/bottom-nav-component.js
@@ -7,33 +7,32 @@ class BottomNavComponent extends Component {
     let previousLink = null, nextLink = null;
     if (this.props.previousPost) {
       previousLink =
-        <Link to={`/posts/${this.props.previousPost.slug}`}>
-          <i class="fas fa-arrow-alt-circle-left"></i>
-          {this.props.previousPost.title}
+        <Link className="previous" to={`/posts/${this.props.previousPost.slug}`}>
+          <i className="fas fa-arrow-alt-circle-left"></i>
+          <span>{this.props.previousPost.title}</span>
         </Link>;
     }
     if (this.props.nextPost) {
       nextLink =
-        <Link to={`/posts/${this.props.nextPost.slug}`}>
-          {this.props.nextPost.title}
-          <i class="fas fa-arrow-alt-circle-right"></i>
+        <Link className="next" to={`/posts/${this.props.nextPost.slug}`}>
+          <span>{this.props.nextPost.title}</span>
+          <i className="fas fa-arrow-alt-circle-right"></i>
         </Link>;
     }
+    let allLink =
+      <Link className="all" to="/posts">
+        <i className="fas fa-caret-square-left"></i>
+        <span>All Posts</span>
+      </Link>
+
     return (
-      <div className="bottom-nav">
-        <div className="arrow-link all">
-          <Link to="/posts">
-            <i class="fas fa-caret-square-left"></i>
-            All Posts
-          </Link>
+      <div className="article-nav">
+        <div className="nav-row">
+          {allLink}
         </div>
-        <div className="previous-next">
-          <div className="arrow-link previous">
-            {previousLink}
-          </div>
-          <div className="arrow-link next">
-            {nextLink}
-          </div>
+        <div className="nav-row previous-next">
+          {previousLink}
+          {nextLink}
         </div>
       </div>
     )
